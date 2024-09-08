82dd65f2570bc3857e49364cbec1733074eb54b0
Danil Zobkov <56187773+ovelwe@users.noreply.github.com>
2024-09-08 13:54:02 +0300

    added .env file

--- config/config.go
+++ config/config.go
@@ -3,15 +3,22 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 var DB *pgx.Conn
 
 func Init() {
+	if godotenv.Load() != nil {
+		log.Fatal("Ошибка загрузки .env файла")
+	}
+	dbURL := os.Getenv("DATABASE_URL") 
 	var err error
-	connStr := "postgres://postgres:123321@localhost:5432/todo-app"
+	
+	connStr := dbURL
 	DB, err = pgx.Connect(context.Background(), connStr)
 	if err != nil {
 		log.Fatalf("Ошибка подключения к БД: %v", err)
